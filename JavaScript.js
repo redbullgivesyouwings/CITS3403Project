66498f73cf91170a97ac52d0b4eb88c2fdcf1438
redbullgivesyouwings <83648638+redbullgivesyouwings@users.noreply.github.com>
2022-05-21 14:04:45 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,9 @@
 'use strict'
 $(window).on('load', () =>  {
-
+    let buttons = document.getElementsByClassName('btn btn-info');
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('mouseclick', function() {
+            $.get()
+        })
+    }
 })
