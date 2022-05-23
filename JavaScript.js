60b1e6fcdc346b83a56eeff6888c238d177754bd
redbullgivesyouwings <83648638+redbullgivesyouwings@users.noreply.github.com>
2022-05-23 13:31:18 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,31 +1,43 @@
 'use strict'
 $(window).on('load', () =>  {
     /* getting day of week and corresponding image */
-    /* lines 5-7 is from https://www.w3schools.com/jsref/jsref_getday.asp */
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    /* lines 5-6 is from https://www.w3schools.com/jsref/jsref_getday.asp */
     const d = new Date();
-    let day = weekday[d.getDay()];
+    let day = d.getDay();
     const images = ["sunday.jpeg", "monday.jpeg", "tuesday.jpeg", "wednesday.jpeg", "thursday.jpeg", "friday.jpeg", "saturday.jpeg"];
     const answers = ["arc de triomphe", "stonehenge", "the sphynx", "the great wall of china", "mount rushmore", "neuschwanstein castle", "saint basil's cathedral"];
-    document.getElementsByTagName("img")[0].src = images[day];
+    document.getElementsByTagName('img')[0].src = images[day];
+
+    /* instructions button functionality */
+    let instructionsButton = document.getElementById('show');
+    let instructions = document.getElementById('instructions');
+    let closeButton = document.getElementById('close');
+    instructionsButton.addEventListener('mouseclick', function() {
+        instructions.show();
+    })
+    closeButton.addEventListener('mouseclick', function() {
+        instructions.hide();
+    })
+    
     /* submit buttons functionality */
     let buttons = document.getElementsByClassName('btn btn-info');
     let guess = document.getElementsByClassName('form-control');
-    attempts = 0;
+    let message = document.getElementsByTagName('h2')[0];
+    let attempts = 0;
+    let statistic = document.getElementsByClassName('label')[0];
+    
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('mouseclick', function() {
             if(guess[i] == answers[day]) {
-                document.getElementsByTagName('h2')[0].innerText = "Congratulations";
+                message.innerText = "Congratulations";
+                let accuracy = (1 - attempts/5) * 100;
+                statistic.innerText = accuracy + "% accuracy";
             }
-            else if(document.getElementsByClassName('input-group-text')[4]) { 
-                document.getElementsByTagName('h2')[0].innerText = "Answer: " + answers[day]; 
+            else {
+                attempts += 1;
+                if(attempts == 5) {message.innerText = "Answer: " + answers[day];}
+                else {message.innerText = "Try Again";}
             }
-            else {document.getElementsByTagName('h2')[0].innerText = "Try Again";}
-            attempts += 1;
         })
     }
-    /* results calculation */
-    let statistic = document.getElementsByClassName('statistic')[0];
-    accuracy = (1 - attempts/5) * 100;
-    statistic.innerText = accuracy + "% accuracy";
 })
